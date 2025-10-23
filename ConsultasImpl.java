526ceb67d9de2f59f3ea4ba1276bfd84f3b51e75
Rafael dos Santos <rafaelmartins_contato@hotmail.com>
2025-10-23 21:40:18 +0000

    feat: criados Repositórios Estáticos com as classes QuadroFuncionarios e ConsultasImpl. O uso da palavra-chave static nos permitiu ter listas globais, que armazenam todos os funcionários e consultas, acessíveis de qualquer ponto da aplicação, sem precisar criar novos objetos.

---
+++ ConsultasImpl.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultasImpl {
+
+    // Propriedade estática que armazena a lista de consultas.
+    // É inicializada automaticamente com um novo ArrayList.
+    public static List<Consulta> listaConsultas = new ArrayList<>();
+}
---
+++ QuadroFuncionario.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuadroFuncionarios {
+
+    // Atributo estático que armazena a lista de funcionários.
+    // Ele é inicializado automaticamente com um novo ArrayList.
+    public static List<Funcionario> listaFuncionarios = new ArrayList<>();
+}
